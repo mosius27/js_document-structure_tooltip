af20f16cf114bea4f4b4f0e529bbbe4806f8f865
mosius27 <mosius27@yandex.ru>
2023-09-25 15:06:26 +0300

    .

--- task.js
+++ task.js
@@ -1,35 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Находим все элементы с классом "has-tooltip"
-    const tooltips = document.querySelectorAll(".has-tooltip");
-  
-    // Перебираем найденные элементы
-    tooltips.forEach((tooltip) => {
-      // Создаем элемент подсказки
-      const tooltipElement = document.createElement("div");
-      tooltipElement.className = "tooltip";
-  
-      // Устанавливаем текст подсказки из атрибута "title"
-      tooltipElement.textContent = tooltip.getAttribute("title");
-  
-      // Скрываем подсказку по умолчанию
-      tooltipElement.style.display = "none";
-  
-      // Добавляем подсказку к элементу
-      tooltip.appendChild(tooltipElement);
-  
-      // Добавляем обработчик события клика на элемент
-      tooltip.addEventListener("click", function (event) {
-        event.preventDefault(); // Предотвращаем переход по ссылке
-  
-        // Переключаем видимость подсказки при клике
-        if (tooltipElement.style.display === "none" || tooltipElement.style.display === "") {
-          tooltipElement.style.display = "block";
-        } else {
-          tooltipElement.style.display = "none";
-        }
-      });
-  
-      // Удаляем атрибут "title", чтобы браузер не показывал стандартную подсказку
-      tooltip.removeAttribute("title");
+  // Находим все элементы с классом "has-tooltip"
+  const tooltips = document.querySelectorAll(".has-tooltip");
+
+  // Перебираем найденные элементы
+  tooltips.forEach((tooltip) => {
+    // Создаем элемент подсказки
+    const tooltipElement = document.createElement("div");
+    tooltipElement.className = "tooltip";
+    tooltipElement.textContent = tooltip.getAttribute("title");
+
+    // Удаляем атрибут "title", чтобы браузер не показывал стандартную подсказку
+    tooltip.removeAttribute("title");
+
+    // Добавляем обработчик события клика на элемент
+    tooltip.addEventListener("click", function (event) {
+      event.preventDefault(); // Предотвращаем переход по ссылке
+
+      // Получаем позицию ссылки
+      const linkRect = tooltip.getBoundingClientRect();
+
+      // Устанавливаем позицию подсказки рядом с ссылкой
+      tooltipElement.style.position = "absolute";
+      tooltipElement.style.left = linkRect.left + "px";
+      tooltipElement.style.top = linkRect.bottom + "px";
+
+      // Добавляем класс для отображения подсказки
+      tooltipElement.classList.toggle("tooltip_active");
     });
+
+    // Добавляем подсказку к body вне ссылки
+    document.body.appendChild(tooltipElement);
   });
+});
